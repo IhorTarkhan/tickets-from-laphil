fe0f1ff769a0bca3f69246166948cf4055dad224
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2023-03-31 12:28:32 +0300

    stream with fp-ts

--- app.ts
+++ app.ts
@@ -4,6 +4,8 @@ import TicketDto from "./dto/laphil.response/TicketDto";
 import SectionDto from "./dto/laphil.response/SectionDto";
 import AvailableTicketDto from "./dto/response/AvailableTicketDto";
 import {Reader} from "fp-ts/Reader"
+import {pipe} from "fp-ts/function"
+import {filter, map} from "fp-ts/Array"
 
 const app = express();
 const port = 3000;
@@ -17,20 +19,27 @@ const sectionsPromise = fetch("https://my.laphil.com/en/rest-proxy/ReferenceData
 
 const handleAvailableTicketsById: Reader<Dependencies, RequestHandler> = (deps: Dependencies) =>
     (request: Request, response: Response) => {
+        const onlyActive = (ticket: TicketDto): boolean => ticket.SeatStatusId === 0
+        const toAvailableResponse = (ticket: TicketDto): AvailableTicketDto => (
+            {
+                // section: pipe(fromNullable(deps.sections.get(ticket.SectionId)), getOrElse(() => "-")),
+                section: deps.sections.get(ticket.SectionId) || "-",
+                row: ticket.SeatRow,
+                seatNumber: ticket.SeatNumber,
+                price: 3.14 // todo
+            }
+        )
+
         const id: string = request.params.performanceId;
         // todo: `modeOfSaleId=4` - why 4? with 0, 1, 2, 3 it is not work
         fetch(`https://my.laphil.com/en/rest-proxy/TXN/Performances/${id}/Seats?constituentId=0&modeOfSaleId=4&performanceId=${id}`)
             .then(res => res.json())
             .then((tickets: TicketDto[]) => {
-                const result = tickets.filter(x => x.SeatStatusId === 0)
-                    .map(item => (
-                        {
-                            section: deps.sections.get(item.SectionId) || "-",
-                            row: item.SeatRow,
-                            seatNumber: item.SeatNumber,
-                            price: 3.2 // todo
-                        } as AvailableTicketDto
-                    ))
+                const result: AvailableTicketDto[] = pipe(
+                    tickets,
+                    filter(onlyActive),
+                    map(toAvailableResponse),
+                )
                 response.status(200).json(result)
             })
     }
