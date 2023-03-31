f819fb7abba8c78f95bffd6d6a5879879503b799
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2023-03-31 16:55:17 +0300

    move dependencies in folder

--- app.ts
+++ app.ts
@@ -1,14 +1,15 @@
 import express, {Request, RequestHandler, Response} from "express";
-import GlobalDependencies from "./GlobalDependencies";
-import LocalDependencies from "./LocalDependencies";
+import GlobalDependencies from "./dependency/GlobalDependencies";
+import LocalDependencies from "./dependency/LocalDependencies";
 import TicketDto from "./dto/laphil.response/TicketDto";
+import PriceDto from "./dto/laphil.response/PriceDto";
 import SectionDto from "./dto/laphil.response/SectionDto";
 import AvailableTicketDto from "./dto/response/AvailableTicketDto";
-import {Reader} from "fp-ts/Reader"
 import {pipe} from "fp-ts/function"
-import {filter, map} from "fp-ts/Array"
-import {fromNullable, getOrElse} from "fp-ts/Option";
-import PriceDto from "./dto/laphil.response/PriceDto";
+import * as O from "fp-ts/Option";
+import * as A from "fp-ts/Array"
+import {Reader} from "fp-ts/Reader"
+import {Predicate} from "fp-ts/Predicate";
 
 const app = express();
 const port = 3000;
@@ -26,14 +27,14 @@ const handleAvailableTicketsById: Reader<GlobalDependencies, RequestHandler> = (
         /**
          * Here getting only `Active` statuses, according this status list: https://my.laphil.com/en/rest-proxy/ReferenceData/SeatStatuses
          */
-        const onlyActive = (ticket: TicketDto): boolean => ticket.SeatStatusId === 0
+        const onlyActive: Predicate<TicketDto> = (ticket: TicketDto): boolean => ticket.SeatStatusId === 0
         const toAvailableResponse = (localDeps: LocalDependencies) =>
             (ticket: TicketDto): AvailableTicketDto => (
                 {
-                    section: pipe(fromNullable(deps.sections.get(ticket.SectionId)), getOrElse(() => "-")),
+                    section: pipe(O.fromNullable(deps.sections.get(ticket.SectionId)), O.getOrElse(() => "-")),
                     row: ticket.SeatRow,
                     seatNumber: ticket.SeatNumber,
-                    price: pipe(fromNullable(localDeps.priceByZone.get(ticket.ZoneId)), getOrElse(() => -1))
+                    price: pipe(O.fromNullable(localDeps.priceByZone.get(ticket.ZoneId)), O.getOrElse(() => -1))
                 }
             )
 
@@ -46,16 +47,16 @@ const handleAvailableTicketsById: Reader<GlobalDependencies, RequestHandler> = (
 
         Promise.all([ticketsPromise, pricesPromise])
             .then(([tickets, prices]: [TicketDto[], PriceDto[]]) => {
-                const priceByZone = new Map(prices.map(p => [p.ZoneId, p.Price]));
-                const localDeps: LocalDependencies = {priceByZone}
-
-                const result: AvailableTicketDto[] = pipe(
-                    tickets,
-                    filter(onlyActive),
-                    map(toAvailableResponse(localDeps)),
-                )
-                response.status(200).json(result)
-            })
+                    const priceByZone = new Map(prices.map(p => [p.ZoneId, p.Price]));
+                    const localDeps: LocalDependencies = {priceByZone};
+                    return response.status(200).json(
+                        pipe(
+                            tickets,
+                            A.filter(onlyActive),
+                            A.map(toAvailableResponse(localDeps)),
+                        ));
+                }
+            )
     }
 
 Promise.all([sectionsPromise]).then(([sections]) => {
